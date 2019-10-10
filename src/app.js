41fa46001a75b8d81d1a3687162d5942e2af1807
Kevvin Sahdo <kevvin@meliuz.com.br>
2019-10-10 13:58:50 -0400

    feat: Adiciona layout

--- src/app.js
+++ src/app.js
@@ -6,16 +6,43 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const morgan = require('morgan');
 const router = require('./routes');
+const sass = require('node-sass-middleware');
 
 const app = express(); 
 
+// Logger
 app.use(morgan('short'));
+
+// Routers
 app.use(router);
 
+// Images files
 app.use('/img', [
     express.static('/public/img')
+
 ])
 
+// Sass
+app.use(sass({
+    src: 'public/scss',
+    dest: 'public/css',
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/css',
+    force: true,
+}))
+
+
+// Static js files
+app.use('/js', [
+    express.static('node_modules/jquery/dist/'), 
+    express.static('node_modules/popper.js/dist/umd/'), 
+    express.static('node_modules/bootstrap/dist/js/'), 
+    express.static('public/js')
+])
+
+
+// Views engine
 app.engine('handlebars', handlebars({
     helpers: require('helpers/handlebars.js'),
     layoutsDir: 'src/views/layouts',
@@ -26,6 +53,6 @@ app.set('view engine', 'handlebars');
 app.set('views', 'src/views');
 
 
-app.listen(3000, () => { 
-    console.log('Express app iniciada na porta 3000.'); 
+app.listen(process.env.APP_PORT, () => { 
+    console.log('Express app iniciada na porta ' + process.env.APP_PORT); 
 });
